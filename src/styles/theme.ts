8a45db0e49b9729919275abb00378a8a3aa17e46
Alan Christian <alanchristiancreator@hotmail.com>
2021-09-23 19:46:50 -0300

    feat: create global ThemeProvider component

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+export interface ITheme {
+  color: {
+    blue: string
+    gray: string
+    darkGray: string
+    borderColor:string
+  },
+}
+
+export const theme: ITheme = {
+  color: {
+    blue: '#1da1f2',
+    gray: '#8899a6',
+    darkGray: '#253343',
+    borderColor: '#2f3336'
+  },
+}
