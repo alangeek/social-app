76d95ad4f0c7a52ca7949a226ba9e0bdb7f1f722
Alan Christian <alanchristiancreator@hotmail.com>
2021-09-24 20:45:23 -0300

    feat: login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,21 @@
+import { BackgroundContainer, Button, Container, RightContainer } from './styles'
+import Background from '../../assets/images/banner-bg.jpg'
+import { FaTwitter } from 'react-icons/fa'
+
 const Login = () => {
   return (
-    <h1>Página de login</h1>
+    <Container>
+      <BackgroundContainer>
+        <img src={Background} alt="Banner login" />
+      </BackgroundContainer>
+      <RightContainer>
+        <FaTwitter color="#d9d9d9" size={40}/>
+        <h1>Acontecendo agora</h1>
+        <h2>Inscreva-se no Twitter hoje mesmo.</h2>
+        <Button>Inscreva-se com e-mail</Button>
+        <p>Já tem uma conta? <span>Entre</span></p>
+      </RightContainer>
+    </Container>
   )
 }
 
---
+++ src/pages/Login/styles.ts
@@ -0,0 +1,63 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+`
+
+export const BackgroundContainer = styled.div`
+  img { 
+    height: 100vh;
+    width: 50vw;
+    object-fit: cover;
+  }
+`
+
+export const RightContainer = styled.div`
+  margin-left: 30px;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+
+  h1 {
+    margin-top: 60px;
+    margin-bottom: 32px;
+    font-weight: 700;
+    font-size: 64px;
+    font-style: normal;
+    line-height: 83px;
+  }
+
+  h2 {
+    font-size: 31px;
+    font-style: normal;
+    font-weight: 700;
+    line-height:40px;
+    margin-bottom: 32px;
+  }
+
+  p {
+    font-size: 16px;
+    font-style: normal;
+    line-height: 19px;
+    margin-top: 26px;
+  }
+
+  span {
+    color: purple;
+    cursor: pointer;
+  }
+`
+
+export const Button = styled.button`
+  background-color: #fff;
+  color: #000;
+  border-radius: 70px;
+  height: 49px;
+  width: 381px;
+  padding: 14px 92px 14px 92;
+  border: none;
+  font-size: 16px;
+  font-weight: 700;
+  cursor: pointer;
+`
